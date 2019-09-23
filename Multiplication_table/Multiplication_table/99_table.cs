919ac5c7c91cf5ebd7104ade3dd6a053d4412d2d
brian <paul.jonez.tw@gmail.com>
2019-09-23 20:32:30 +0800

    Added 9*9 table

---
+++ Multiplication_table/Multiplication_table/99_table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int outer;
+            int inner;
+            Console.WriteLine("9 * 9 Table");
+            for(outer=1;outer<=9;outer++)
+            {
+                for(inner=1;inner<=9;inner++)
+                {
+                    Console.Write("[{0,2} *{1,2} ={2,2}] ", inner, outer, inner * outer);
+                }
+                Console.WriteLine();            
+            }
+            Console.Read();
+        }
+    }
+}
